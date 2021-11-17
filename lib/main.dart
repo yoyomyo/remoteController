02db36395a8b96392f458cfbfe008c6389a890cd
Yun Miao <one.missing.variable@gmail.com>
2021-11-17 12:26:16 -0800

    add RTDB

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 import 'player_widget.dart';
 
